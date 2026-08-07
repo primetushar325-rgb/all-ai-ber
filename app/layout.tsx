fa6b382c1f077cee08a62c958a68f73ba5ed2792
primetushar325-rgb <primetushar325@gmail.com>
2026-08-08 02:06:54 +0600

    Add vignette and onclick ad zones

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {ADSENSE_CONFIG.enabled && ADSENSE_CONFIG.clientId && (
           <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CONFIG.clientId}`} crossOrigin="anonymous"></script>
         )}
+    <script>(function(s){s.dataset.zone='11527321',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+    <script>(function(s){s.dataset.zone='11527319',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
       </head>
       <body className="min-h-screen bg-[#fafafa] flex flex-col">
         <Navbar />
